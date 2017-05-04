939ecd2f1331866958290b4004d6923f6b62321b
1istbesser <tamer.altintop@gmail.com>
2017-05-04 16:57:55 +0100

    Add files via upload

---
+++ MainClass.java
@@ -0,0 +1,22 @@
+/**
+* Tamerincode database tool is an application that
+* provides you with a dynamic way of adding, deleting and updating
+* records in a MySQL database.
+* @author  Tamer Altintop, student id s4908098
+* @version 1.0
+* @since 25/05/2017
+*/
+
+public class MainClass {
+	
+	/**
+	 * The main method. It calls the LogicWindow class as soon as the application starts.
+	 *
+	 * @param args - the standard arguments of the method
+	 * @throws ClassNotFoundException 
+	 * @throws SQLException 
+	 */
+	public static void main(String[] args) {
+		new LoginWindow();
+	}
+}
