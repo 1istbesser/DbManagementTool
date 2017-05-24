dbf9322bd8335987c1db8d110b9d3bbc4c35d92a
1istbesser <tamer.altintop@gmail.com>
2017-05-25 00:56:32 +0100

    Removing Javadoc comments.

--- src/LoginWindow.java
+++ src/LoginWindow.java
@@ -27,104 +27,33 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
-/**
- * This class creates the first window seen by the user
- * upon starting the application. Its purpose <br>is to gather
- * the information about the database to which the user
- * wishes to connect to.<br><br>
- * 
- * Once the information has been gathered, it tests the connection<br>
- * and if the connection is successful, it calls the ApplicationWindow class<br>
- * passing as parameters the username, password, the database name and the host.
- * 
- */
 
 public class LoginWindow {	
 	
-	/** The declaration of the frame. */
+
 	private JFrame frame;
 	
-	/** The declaration of the panels.<br>
-	 * <ol>
-	 * <li><b>panel</b> - The main panel.</li>
-	 * <li><b>panelMainCenter</b> - The sub-panel of panel taking up the center area.</li>
-	 * <li><b>panelCenter</b> - The sub-panel of panelMainCenter taking up the center area.</li>
-	 * <li><b>panelTop</b> - The sub-panel taking up the top area.</li>
-	 * <li><b>panelBottom</b> - The sub-panel taking up bottom area.</li>
-	 * </ol>*/
+
 	private JPanel panel, panelCenterMain, panelCenter, panelTop, panelBottom;
-	
-	/** The declaration of the icon images. */
+
 	private ImageIcon dbImage = null;
-	
-	/** The declaration of the labels.<br>
-	 * <ol>
-	 * <li><b>lblWelcome</b> - The label for the welcome message.</li>
-	 * <li><b>lblUsername</b> - The label for the username text field.</li>
-	 * <li><b>lblPassword</b> - The label for the password text field.</li>
-	 * <li><b>lblHost</b> - The label for the host text field.</li>
-	 * <li><b>lblDatabase</b> - The label for the database text field.</li>
-	 * <li><b>lblAdditional</b> - The label for the additional information message.</li>
-	 * </ol> */
+
 	private JLabel lblWelcome, lblUsername, lblPassword, lblHost, lblDataBase, lblAdditional;
-	
-	/** The declaration of the buttons.<br>
-	 * <ol>
-	 * <li><b>btnConnect</b> - The button to connect to the database.</li>
-	 * </ol> */
+
 	private JButton btnConnect;
-	
-	/** The declaration of the text fields.<br>
-	 * <ol>
-	 * <li><b>tfUsername</b> - The text field for the username parameter.</li>
-	 * <li><b>tfHost</b> - The text field for the host parameter.</li>
-	 * <li><b>tfDatabase</b> - The text field for the database parameter.</li>
-	 * </ol> */
+
 	private JTextField tfUsername, tfHost, tfDataBase;
-	
-	/** The declaration of the password field 
-	 * <ol>
-	 * <li><b>pfPassword</b> - The password field for the password parameter.</li>
-	 * </ol> */
+
 	private JPasswordField pfPassword;
-	
-	/** The declaration of the String variables used to store the value of the database connection parameters.
-	 * <ol>
-	 * <li><b>username</b> - The string containing the username value.</li>
-	 * <li><b>password</b> - The string containing the password value.</li>
-	 * <li><b>database</b> - The string containing the database value.</li>
-	 * <li><b>host</b> - The string containing the host value.</li>
-	 * </ol> */
+
 	private String username, host, database, password;
-	
-	/** The declaration of the Grid Bag Constraint.<br>
-	 * <b>c</b> - The Grid Bag Constraint. Its name is kept as short as possible for
-	 * it is widly used throughout the application. */
+
 	private GridBagConstraints c = new GridBagConstraints();
-	
-	/** The declaration and initialization of the custom colors.<br>
-	 * <b>blueish</b> - The custom color with the rgb(34, 153, 183). */
+
 	private Color blueish = new Color(34, 153, 183);
 	
-	/**
-	 * Creating a new DatabaseOperations object.
-	 */
 	private DatabaseOperations dbOps = new DatabaseOperations();
 	
-	/**
-	 * Instantiates a new login window.<br><br>
-	 * <b>List of contained methods</b>
-	 * <ol>
-	 * <li>{@link #createFrame}</li>
-	 * <li>{@link #createPanel}</li>
-	 * <li>{@link #addImages}</li>
-	 * <li>{@link #addLabels}</li>
-	 * <li>{@link #addTextFields}</li>
-	 * <li>{@link #addButtons}</li>
-	 * <li>{@link #setConnectButtonListener}</li>
-	 * </ol>
-	 * 
-	 */
 	public LoginWindow(){
 		createFrame();
 		createPanel();
@@ -136,10 +65,7 @@ public class LoginWindow {
 		frame.add(panel);
 		frame.setVisible(true);
 	}
-	
-	/**
-	 * Creates the frame and sets the frame properties like size and title.
-	 */
+
 	private void createFrame(){
 		frame = new JFrame();
 		frame.setTitle("Tamerincode ~ A database tool V1.1");
@@ -147,16 +73,6 @@ public class LoginWindow {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
 	}
 	
-	/**
-	 * Creates the main panel and the sub-panels. It also sets up the properties of each panel or sub-panel and it adds each panel<br>
-	 * to their parent panel.
-	 * <ol>
-	 * <li><b>panel</b> - The main panel.</li>
-	 * <li><b>panelMainCenter</b> - The sub-panel of panel taking up the center area.</li>
-	 * <li><b>panelCenter</b> - The sub-panel of panelMainCenter taking up the center area.</li>
-	 * <li><b>panelTop</b> - The sub-panel taking up the top area.</li>
-	 * <li><b>panelBottom</b> - The sub-panel taking up bottom area.</li>
-	 * </ol>*/
 	private void createPanel(){
 		//Initialization
 		panel = new JPanel(new BorderLayout());
@@ -177,19 +93,13 @@ public class LoginWindow {
 		panelCenterMain.add(panelCenter, BorderLayout.CENTER);
 	}
 
-	/**
-	 * This method instantiates the images and adds them to the corresponding panel.
-	 */
 	private void addImages(){
 		
 		dbImage = new ImageIcon(this.getClass().getClassLoader().getResource("db4.png"));
 		JLabel lblDatabaseImage = new JLabel(dbImage);
 		panelCenterMain.add(lblDatabaseImage, BorderLayout.NORTH);
 	}
-	
-	/**
-	 * This method instantiates the labels and adds them to the corresponding panel using the Grid Bag Constraints.
-	 */
+
 	private void addLabels(){
 		//Instantization
 		lblWelcome = new JLabel("<html><span style='color:white;font-size:1.5em;'>Hello and welcome to this simple database management tool!</span></html>");
@@ -235,10 +145,6 @@ public class LoginWindow {
 		panelCenter.add(lblHost, c);
 	}
 
-	/**
-	 * This method instantiates the text fields, password fields. It is also setting up the properties of the text fields<br>
-	 * and it adds them to the panels.
-	 */
 	private void addTextFields(){
 		//Instantization
 		tfUsername = new JTextField("increible");
@@ -268,10 +174,7 @@ public class LoginWindow {
 		c.gridy=2;
 		panelCenter.add(tfHost, c);
 	}
-	
-	/**
-	 * This method instantiates the buttons and it adds them to the panels.
-	 */
+
 	private void addButtons(){
 		//Instantization
 		btnConnect = new JButton("Connect");
@@ -282,13 +185,7 @@ public class LoginWindow {
 		c.gridy=5;
 		panelCenter.add(btnConnect, c);
 	}
-	
-	/**
-	 * This method sets the listener for the 'Connect' button. The database connection is tested<br>
-	 * with the parameters provided by the user through the text fields through a DatabaseOperations method..<br><br>
-	 * If the connection is successful, the current window is <b>disposed</b>,<br>
-	 * and the <b>ApplicationWindow</b> is invoked.<br><br>
-	 */
+
 	private void setConnectButtonListener() {
 		btnConnect.addActionListener(new ActionListener() {
 			@SuppressWarnings("deprecation")
--- src/MainClass.java
+++ src/MainClass.java
@@ -10,11 +10,6 @@
 */
 
 public class MainClass {
-	
-	/**
-	 * The main method. It calls the LogicWindow class as soon as the application starts.
-	 * @param args - the standard arguments of the method
-	 */
 	public static void main(String[] args) {
 		new LoginWindow();
 	}
